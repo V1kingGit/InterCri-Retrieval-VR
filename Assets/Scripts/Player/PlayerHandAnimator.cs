fa5c5b46a652dd415a853782352b9fc5b3679b2c
Frederik Gonge <45792897+V1kingGit@users.noreply.github.com>
2021-12-04 13:12:57 +0100

    Gun grabbing, Hand anim work

--- Assets/Scripts/Player/PlayerHandAnimator.cs
+++ Assets/Scripts/Player/PlayerHandAnimator.cs
@@ -2,21 +2,25 @@ using UnityEngine;
 using UnityEngine.XR;
 using System.Collections.Generic;
 
-public class VRHandAnimator : MonoBehaviour
+public class PlayerHandAnimator : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private Animation[] handAnimations = null;
+    [SerializeField] private Animator[] animators = null;
     [Header("Values")]
-    [SerializeField] private string[] animNames = null;
+    [SerializeField] private float animSpeed = default;
 
     private InputDevice[] vrControllers = new InputDevice[2]; // Left is 0, Right is 1
+
+    private float[] triggerCurrent = new float[2];
+    private float[] gripCurrent = new float[2];
+    private int[] thumbCurrent = new int[2];
 
     private void Start()
     {
         InitVRInputs();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         UpdateVRInputs();
     }
@@ -39,9 +43,9 @@ public class VRHandAnimator : MonoBehaviour
     {
         for(int i = 0; i < vrControllers.Length; ++i)
         {
+            vrControllers[i].TryGetFeatureValue(CommonUsages.trigger, out float trigger);
             vrControllers[i].TryGetFeatureValue(CommonUsages.grip, out float grip);
             vrControllers[i].TryGetFeatureValue(CommonUsages.gripButton, out bool gripButton);
-            vrControllers[i].TryGetFeatureValue(CommonUsages.trigger, out float trigger);
             vrControllers[i].TryGetFeatureValue(CommonUsages.secondary2DAxisTouch, out bool joystickTouch);
             vrControllers[i].TryGetFeatureValue(CommonUsages.secondary2DAxisClick, out bool joystickClick);
 
@@ -54,8 +58,33 @@ public class VRHandAnimator : MonoBehaviour
                 //movement = primary2DAxis;
             }
             // Both
-            //handAnimations[i].Play(,);
-            Animator animator;
+            int thumbTarget = 0;
+            if(joystickClick)
+                thumbTarget = 2;
+            else if(joystickTouch)
+                thumbTarget = 1;
+            AnimateHand(i, trigger, grip, thumbTarget);
+        }
+    }
+
+    private void AnimateHand(int index, float triggerTarget, float gripTarget, int thumbTarget)
+    {
+        if(gripCurrent[index] != gripTarget)
+        {
+            gripCurrent[index] = Mathf.MoveTowards(gripCurrent[index], gripTarget, Time.deltaTime * animSpeed);
+            animators[index].SetFloat("Grip", gripCurrent[index]);
+        }
+
+        if(triggerCurrent[index] != triggerTarget)
+        {
+            triggerCurrent[index] = Mathf.MoveTowards(triggerCurrent[index], triggerTarget, Time.deltaTime * animSpeed);
+            animators[index].SetFloat("Trigger", triggerCurrent[index]);
+        }
+
+        if(thumbCurrent[index] != thumbTarget)
+        {
+            thumbCurrent[index] = thumbTarget;
+            animators[index].SetInteger("Thumb", thumbCurrent[index]);
         }
     }
 }
